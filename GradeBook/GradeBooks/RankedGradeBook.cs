3f3d48f6c453bb0e25657a745b2a3df0323a6415
mihuwis <mihuwis@gmail.com>
2024-03-13 13:48:17 +0100

    Add RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook:BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            base.Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
